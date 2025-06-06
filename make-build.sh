38649e6b88e8b03348c4e2730aadcbba89b617a9
aday-allbase <maxroepz48@gmail.com>
2025-06-07 06:33:57 +0700

    aday

--- make-build.sh
+++ make-build.sh
@@ -10,16 +10,16 @@ PACKAGES=""
 # Base packages
 PACKAGES+=" -dnsmasq dnsmasq-full cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua \
 luci-base luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
-cpusage ttyd dmesg kmod-tun luci-lib-ipkg git git-http \
+ttyd dmesg kmod-tun luci-lib-ipkg git git-http \
 zram-swap adb parted losetup resize2fs luci luci-ssl block-mount htop bash curl wget-ssl \
 tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server \
 liblucihttp liblucihttp-lua libubus-lua lua luci-app-firewall luci-app-opkg \
-ca-bundle ca-certificates luci-compat coreutils-sleep fontconfig coreutils-whoami file lolcat \
+ca-bundle ca-certificates luci-compat coreutils-sleep fontconfig coreutils-whoami file \
 luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp \
 px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci \
 rpcd-mod-rrdns uhttpd uhttpd-mod-ubus coreutils coreutils-base64 coreutils-nohup coreutils-stty libc coreutils-stat coreutils-timeout \
-ip-full libuci-lua resolveip ipset iptables iptables-legacy \
-iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat luci-lua-runtime zoneinfo-asia zoneinfo-core \
+ip-full libuci-lua resolveip ipset iptables \
+iptables-mod-iprange iptables-mod-tproxy kmod-ipt-nat zoneinfo-asia zoneinfo-core \
 perl perlbase-base perlbase-bytes perlbase-class perlbase-config perlbase-cwd perlbase-dynaloader perlbase-errno perlbase-essential perlbase-fcntl perlbase-file \
 perlbase-filehandle perlbase-i18n perlbase-integer perlbase-io perlbase-list perlbase-locale perlbase-params perlbase-posix \
 perlbase-re perlbase-scalar perlbase-selectsaver perlbase-socket perlbase-symbol perlbase-tie perlbase-time perlbase-unicore perlbase-utf8 perlbase-xsloader"
